8c5ff84793ca9100d0751f6ec20a6a9d5dfa8336
suki-san <csmandrew@live.com>
2025-06-01 15:29:42 +0300

    +

--- steam/build/create.sh
+++ steam/build/create.sh
@@ -48,7 +48,7 @@ mangohud thunderbird gimp audacity lib32-mangohud kodi ksysguard ark kate okular
 qt5-wayland xorg-server-xephyr openbox binutils emby-theater yt-dlp \
 xdotool xbindkeys gparted vlc smplayer mpv fish zsh xmlstarlet nvtop duf exa systemsettings dolphin  \
 legendary gamescope  minizip flatpak libreoffice rsync sdl_mixer sdl_image sdl_ttf sdl_gfx \
-xorg-server xorg-xinit xorg-xrandr xorg-xhost xorg-xdpyinfo github-cli libwebp pcem \
+xorg-server xorg-xinit xorg-xrandr xorg-xhost xorg-xdpyinfo github-cli libwebp  \
 ripgrep i7z sd bandwhich tre zoxide p7zip atop iftop sysstat totem feh krename davinci-resolve  \
 bottles bauh flatseal rebuild-detector ccache axel breeze xorg-xdpyinfo dua-cli  \
 handbrake tigervnc remmina  kitty terminator xorg-xkill media-downloader file  noto-fonts ttf-ubuntu-font-family ttf-droid \
