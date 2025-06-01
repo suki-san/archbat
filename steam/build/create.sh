893cba3f4e485699fd34c072d112bdf0021e5feb
suki-san <csmandrew@live.com>
2025-06-01 16:34:11 +0300

    +

--- steam/build/create.sh
+++ steam/build/create.sh
@@ -53,7 +53,7 @@ ripgrep i7z sd bandwhich tre zoxide p7zip atop iftop sysstat totem feh krename d
 bottles bauh flatseal rebuild-detector ccache axel breeze xorg-xdpyinfo dua-cli  \
 handbrake tigervnc remmina  kitty terminator xorg-xkill media-downloader file  noto-fonts ttf-ubuntu-font-family ttf-droid \
 docker docker-compose portainer-bin unzip gthumb nmon thunar nemo umu-launcher gtk-engines gtk-engine-murrine  \
-gdk-pixbuf-xlib gdk-pixbuf2 xarchiver mc vifm fd deckctl steam-tweaks deck-pref-gui   \
+gdk-pixbuf-xlib gdk-pixbuf2 xarchiver mc vifm fd deckctl steam-tweaks deck-pref-gui ffmpeg handbrake   \
 steam-boilr-gui btrfs-assistant protontricks-git lib32-sdl12-compat sdl12-compat appimagepool-appimage  kmod pciutils xrdp x11vnc tigervnc onboard remmina vinagre freerdp sunshine btrfs-progs tre \
 podman distrobox cheese filezilla dos2unix wmctrl xorg-xprop fzf scc yarn sdl2 sdl2_image squashfs-tools \
 btrfs-heatmap meld lynx yq xorg xorg-server-xvfb nodejs npm cairo-dock imagemagick strace sdl2_mixer python-pysdl2 \
@@ -65,7 +65,7 @@ libretro-gambatte libretro-melonds libretro-mgba libretro-nestopia libretro-para
 libretro-retrodream libretro-yabause pcsx2-git cemu-git citron-git ryujinx shadps4-git 86box \
 screenfetch glances discord jre8-openjdk gcc13 python-pip xfce4 xfce4-goodies obs-studio-git lua53 youtube-music-bin"
 # If you want to install AUR packages, specify them in this variable
-export aur_packagelist="geforcenow-electron mcpelauncher-appimage parsec-bin protonup-qt greenlight-bin azahar-appimage rpcs3-bin  86box-roms 86box-manager-bin sgdboop-bin winegui-bin icu66-bin libffi7 \
+export aur_packagelist="geforcenow-electron mcpelauncher-appimage parsec-bin protonup-qt greenlight-bin azahar-appimage rpcs3-bin  86box-roms 86box-manager-bin sgdboop-bin winegui-bin icu66-bin davinci-resolve \
  zoom"
 
 # ALHP is a repository containing packages from the official Arch Linux
