66b88257ae6b380a23e9f3f0c6c0035a30661527
suki-san <csmandrew@live.com>
2025-06-12 18:17:20 +0300

    +

--- steam/build/create.sh
+++ steam/build/create.sh
@@ -62,7 +62,7 @@ protontricks-git sublime-text-4 fuse2 heroic-games-launcher-bin moonlight-qt ven
 microsoft-edge-stable-bin qdirstat peazip jq steam-rom-manager-git google-chrome steamtinkerlaunch xfwm4 xfwm4-themes \
 prismlauncher retroarch-assets-ozone libretro-beetle-psx-hw bretro-blastem libretro-bsnes libretro-dolphin duckstation  \
 libretro-gambatte libretro-melonds libretro-mgba libretro-nestopia libretro-parallel-n64 libretro-picodrive libretro-ppsspp \
-libretro-retrodream libretro-yabause pcsx2-git cemu-git citron-git ryujinx shadps4-git 86box cinnamon\
+libretro-retrodream libretro-yabause pcsx2-git cemu-git citron-git ryujinx shadps4-git 86box cinnamon-git \
 screenfetch glances discord jre8-openjdk gcc13 python-pip xfce4 xfce4-goodies obs-studio-git lua53 youtube-music-bin"
 # If you want to install AUR packages, specify them in this variable
 export aur_packagelist="geforcenow-electron mcpelauncher-appimage parsec-bin protonup-qt azahar-appimage rpcs3-bin  86box-roms 86box-manager-bin sgdboop-bin winegui-bin  icu66-bin  \
